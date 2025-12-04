163c4942df77226140b0e93550bedbc77ea41555
Yuki Kishimoto <yukikishimoto@protonmail.com>
2025-12-04 11:06:46 +0100

    Update `USER_AGENT_NAME` to use `CARGO_PKG_NAME`

--- src/constant.rs
+++ src/constant.rs
@@ -1,4 +1,6 @@
 pub(crate) const API_ROOT_URL: &str = "https://api.bitfinex.com/v2";
 pub(crate) const API_SIGNATURE_PATH: &str = "/api/v2/auth/r/";
 
-pub(crate) const USER_AGENT_NAME: &str = concat!("strike-api/", env!("CARGO_PKG_VERSION"));
+/// User Agent for the client
+pub(super) const USER_AGENT_NAME: &str =
+    concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
